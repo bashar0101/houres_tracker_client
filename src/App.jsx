1a62ccfb1020206b72860f34ba0609ce503dafe7
bashar <basharhas1999@gmail.com>
2026-02-07 03:31:57 +0300

    save changes to depolyment using vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import ManagerDashboard from './pages/ManagerDashboard';
